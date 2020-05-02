44b0aff73097c14ee522097043d08a7d00bb36d3
joelhochstetter <joel.hochstetter97@gmail.com>
2020-05-02 16:56:35 +1000

    Update files

--- Experiments/Chaos/attractorsToLyapunov.m
+++ Experiments/Chaos/attractorsToLyapunov.m
@@ -1,14 +1,28 @@
-function attractorsToLyapunov(idx, numAtt, attractorFolder, lyFolder)
-    %convert idx to attIdx and runIdx
-    %1-numAtt is jn 1, numAtt+1:2*numAtt for jn 2
-    attIdx  = mod(idx,numAtt);
-    attIdx(attIdx ==0) = numAtt;
+function attractorsToLyapunov(idx, numAtt, attractorFolder, lyFolder, runMode)
+    %{
+        runMode = 0 one for every file
+        runMode = 1, enter idx = 1:E where E is number of particles
+    %}
 
-    runIdx = ceil(idx/numAtt);
+    if nargin == 4
+        runMode = 0;
+    end
     
-    addpath(genpath(attractorFolder));    
+    addpath(genpath(attractorFolder));
     files = dir(strcat(attractorFolder, '/*.mat'));
     
-    calcLyapunovV5(0, runIdx, attractorFolder, files(attIdx).name, lyFolder, 0)
-
+    if runMode == 0
+        %convert idx to attIdx and runIdx
+        %1-numAtt is jn 1, numAtt+1:2*numAtt for jn 2
+        attIdx  = mod(idx,numAtt);
+        attIdx(attIdx ==0) = numAtt;
+        runIdx = ceil(idx/numAtt);
+        %calc
+        calcLyapunovV5(0, runIdx, attractorFolder, files(attIdx).name, lyFolder, 0)
+    elseif runMode == 1
+        for attIdx = 1:numAtt
+            calcLyapunovV5(0, idx, attractorFolder, files(attIdx).name, lyFolder, 0)
+        end            
+    end
+    
 end
