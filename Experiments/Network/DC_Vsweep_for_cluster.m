c3786e0483b0bd73a486c3fb4c48354837c2c47d
joelhochstetter <joel.hochstetter97@gmail.com>
2020-06-24 12:51:24 +1000

    Update files

--- Experiments/Network/DC_Vsweep_for_cluster.m
+++ Experiments/Network/DC_Vsweep_for_cluster.m
@@ -1,4 +1,4 @@
-function DC_Vsweep_for_cluster(idx, saveFolder, minAmp, maxAmp, stepAmp, connFile, initStateFile , initStateFolder, contactDistance, T, saveFilState)
+function DC_Vsweep_for_cluster(idx, saveFolder, minAmp, maxAmp, stepAmp, connFile, initStateFile , initStateFolder, contactDistance, T, saveFilState, rescalePLength, initCon)
 %{
     e.g. usuage
     attractorForCluster(1, 'simulations/InitStateLyapunov/Attractors/', 'simulations/InitStateLyapunov/Lyapunov/', 'ACsaw', 0.2:0.05:0.4,  [0.1, 0.25, 0.5, 0.75, 1.0], 't2_T0.75_DC0.2V_s0.01_r0.01_c0.01_m0.015_b10_p0.mat')
@@ -7,19 +7,31 @@ function DC_Vsweep_for_cluster(idx, saveFolder, minAmp, maxAmp, stepAmp, connFil
 
 %}
 
+if nargin < 13
+    initCon = -1;
+end
+
 if nargin < 6
     connFile = '2016-09-08-155153_asn_nw_00100_nj_00261_seed_042_avl_100.00_disp_10.00.mat';
 end
 
 if nargin < 7 || (isnumeric(initStateFile) && initStateFile == 0)
     initLamda = 0;
+    nameComment = '';
 else
     if nargin == 7
         initStateFolder = saveFolder;
     end
     sim = multiImport(struct('SimOpt', struct('saveFolder', initStateFolder), 'importByName', initStateFile, 'importStateOnly', true));
     if isfield(sim{1}, 'swLam')
-        initLamda                  = sim{1}.swLam(end,:)';
+        if initCon < 0
+            initLamda = sim{1}.swLam(end,:)';
+            nameComment = strcat('_initCon', num2str(sim{1}.netC(end)), 'S');
+        else
+            [~, tidx] = min(abs(sim.netC - initCon));
+            initLamda = sim{1}.swLam(tidx,:)';
+            nameComment = strcat('_initCon', num2str(sim{1}.netC(tidx)), 'S');
+        end
     elseif isfield(sim{1}, 'finalStates')
         initLamda                  = sim{1}.finalStates';
     else
@@ -44,6 +56,13 @@ if nargin < 11
 else
     saveFilState = boolean(saveFilState);
 end
+
+if nargin < 12
+    rescalePLength = false;
+end
+
+
+
 
 %%
 params = struct();
@@ -73,6 +92,20 @@ params.SimOpt.ContactGraphDist = contactDistance;
 params.Stim.BiasType     = 'DC'; % 'ACsaw'; % 'DC' \ 'AC' \ 'DCandWait' \ 'Ramp' \ 'ACsaw'
 params.Stim.Amplitude    = minAmp:stepAmp:maxAmp; %0.2:0.05:0.4; 
 
+%Set connect file
+params.Conn.filename = connFile;
+
+if rescalePLength
+    [Connectivity] = getConnectivity(params.Conn);
+    SimulationOptions = selectContacts(Connectivity, params.SimOpt);
+    Contacts = SimulationOptions.ContactNodes;
+    d = distances(graph(Connectivity.weights), Contacts(1), Contacts(2));
+    params.Stim.Amplitude = d*params.Stim.Amplitude;
+end
+
+params.SimOpt.nameComment = nameComment;
+
+
 %Set Components
 params.Comp.ComponentType  = 'tunnelSwitchL'; %tunnelSwitch2
 params.Comp.onResistance   = 7.77e-5;
@@ -85,8 +118,7 @@ params.Comp.penalty        =    1;
 params.Comp.boost          =   2;
 params.Comp.filamentState = initLamda;
 
-%Set connect file
-params.Conn.filename = connFile;
+
 
 %%%
 multiRun(params);
--- Experiments/Network/DC_sweep_vary_netsVG.m
+++ Experiments/Network/DC_sweep_vary_netsVG.m
@@ -1,15 +1,15 @@
-function DC_sim_vary_nets(idx, netFolder, saveFolder) 
+function DC_sweep_vary_netsVG(idx, netFolder, saveFolder, numNets) 
     
     nets = dir(strcat(netFolder, '/*.mat'))';
-    numNets = 300;
+%     numNets = 300;
     netIdx = mod((idx-1), numNets) + 1; 
     Vidx    = floor((idx-1)/numNets) + 1;
-    saveF1 = strcat(saveFolder, '/seed', num2str(netIdx - 1,'%03.f'), '/');
+    saveF1 = strcat(saveFolder, '/NWN', nets(netIdx).name, '/');
     mkdir(saveF1)
-    DC_Vsweep_for_cluster(Vidx, saveF1, 45*1.25*0.01, 45*1.25*0.01, 45*0.25*0.01, nets(netIdx).name, 0 , '.', 45)
+%     DC_Vsweep_for_cluster(Vidx, saveF1, 45*1.25*0.01, 45*1.25*0.01, 45*0.25*0.01, nets(netIdx).name, 0 , '.', 45)
 %      DC_Vsweep_for_cluster(Vidx, saveF1, 45*5*0.01, 2.5, 1.0, nets(netIdx).name, 0 , '.', 45, 10, 0)
  %    DC_Vsweep_for_cluster(Vidx, saveF1, 45*0.25*0.01, 45*2.00*0.01, 45*0.25*0.01, nets(netIdx).name, 'tl_T10_DC2.25V_s0.01_r0.005_c0.01_m0.015_b2_p1.mat', saveF1, 45, 45, 1)
-%     DC_Vsweep_for_cluster(Vidx, saveF1, 45*1.25*0.01, 45*1.25*0.01, 45*0.25*0.01, nets(netIdx).name, 0 , '.', 45, 1000, 0)
+    DC_Vsweep_for_cluster(Vidx, saveF1, 0.5*0.01, 3.0*0.01, 0.025*0.01, nets(netIdx).name, 0 , '.', -1, 600, true)
      
      
 end
