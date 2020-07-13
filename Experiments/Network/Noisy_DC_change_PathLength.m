bf2e1430da1acaba2cf01569a0eae1f0d2546b02
Joel Hochstetter <joel.hochstetter97@gmail.com>
2020-07-13 21:35:24 +1000

    Update files

--- Experiments/Network/Noisy_DC_change_PathLength.m
+++ Experiments/Network/Noisy_DC_change_PathLength.m
@@ -1,9 +1,13 @@
-function Noisy_DC_change_PathLength(idx, saveFolder,connFile) 
-    for contactDistance = 5:15:65
-        saveF1 = strcat(saveFolder, '/sd', num2str(contactDistance));
+function Noisy_DC_change_PathLength(idx, saveFolder,connFile)
+    cds = 5:15:65;
+    N = numel(cds);
+    cdidx = mod((idx-1), N) + 1; 
+    Vidx    = floor((idx-1)/N) + 1;    
+    for i = cdidx %1:numel(cds)
+        contactDistance = cds(i);
+        saveF1 = strcat(saveFolder, '/sd', num2str(contactDistance), '/');
         mkdir(saveF1)
-        %modify to use noisy
-        DC_Vsweep_for_cluster(idx, saveF1, 1e-2*0.5, 1e-2*3.0, 1e-2*0.05, connFile, 0 , '', contactDistance, 800, 0, true, 0)
+        NoisyDC_Vsweep_for_cluster(Vidx, saveF1, 1e-2*1,  1e-2*1.05, 1e-2*0.05, connFile, 0 , '', contactDistance, 1e4, 5e-3, 1, 0, 1)
     end
 
 end
