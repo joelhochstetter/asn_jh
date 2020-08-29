fbde7fa395bd37c6d509e79bcf13d98cbf11ae3c
joelhochstetter <joel.hochstetter97@gmail.com>
2020-08-29 13:08:59 +1000

    Update files

--- Experiments/Network/DC_sim_vary_nets.m
+++ Experiments/Network/DC_sim_vary_nets.m
@@ -1,14 +1,18 @@
-function DC_sim_vary_nets(idx, netFolder, saveFolder, usePoint, saveEvents) 
+function DC_sim_vary_nets(idx, netFolder, saveFolder, usePoint, saveEvents, useUncorrelated) 
 
     if nargin < 5
         saveEvents = true;
+    end
+    
+    if nargin < 6
+        useUncorrelated = false;
     end
 
     numNets = 100;
     netIdx = mod((idx-1), numNets) + 1; 
     Vidx    = floor((idx-1)/numNets) + 1;
     seed = netIdx - 1;
-    nets = dir(strcat(netFolder, '/*_seed_', num2str(seed,'%03.f'), '*.mat'))';
+    nets = dir(strcat(netFolder, '/*_seed_', num2str(seed,'%03.f'), '*.mat'));
     netName = nets(1).name;
     saveF1 = strcat(saveFolder, '/seed', num2str(seed,'%03.f'), '/');
     mkdir(saveF1)
@@ -19,7 +23,7 @@ function DC_sim_vary_nets(idx, netFolder, saveFolder, usePoint, saveEvents)
         DC_Vsweep_for_cluster(Vidx, saveF1,0.50*0.01,2.00*0.01,0.25*0.01, netName,0, '.', 45,50,true,true,-1,1,false,0,'',0,true)
     else
 %         DC_Vsweep_for_cluster(Vidx, saveF1, 0.7*0.01, 1.8*0.01, 0.1*0.01, nets(netIdx).name, 0 , '.', -1, 30, true, true, -1, 1, true, 0.015, '', 1, true); 
-        DC_Vsweep_for_cluster(Vidx, saveF1, 1.05*0.01, 1.1*0.01, 0.1*0.01, netName, 0 , '.', -1, 30, true, true, -1, 1, true, 0.015, '', 1, saveEvents); 
+        DC_Vsweep_for_cluster(Vidx, saveF1, 1.05*0.01, 1.1*0.01, 0.1*0.01, netName, 0 , '.', -1, 30, true, true, -1, 1, true, 0.015, '', 1, saveEvents, useUncorrelated); 
     end
 %     DC_Vsweep_for_cluster(Vidx, saveF1, 45*1.25*0.01, 45*1.25*0.01, 45*0.25*0.01, nets(netIdx).name, 0 , '.', 45, 1000, 0)
      
