897a1da33f3d634a8894c13d37f0b70ed21f825d
Joel Hochstetter <joel.hochstetter97@gmail.com>
2020-09-02 10:50:59 +1000

    Update files

--- experiments/avalanche/compAvalanche.m
+++ experiments/avalanche/compAvalanche.m
@@ -288,7 +288,7 @@ function compAvalanche(baseFolder, vals, varName, subtype, binSize)
         mkdir(saveFolder);
 
         %% Comparison by parameter
-        binSize(1) = meanIEI(j,1);
+        binSize(binSize < 0) = binSize(binSize < 0)*meanIEI(j,1);
 
         %% Size
         figure('visible', 'off');
