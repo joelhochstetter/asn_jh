71d256823065be4d9cb8e60d6b6125dc4cfc9546
joelhochstetter <joel.hochstetter97@gmail.com>
2020-09-04 09:33:23 +1000

    Update files

--- analysis/avalanche/binSequence.m
+++ analysis/avalanche/binSequence.m
@@ -1,1 +1,20 @@
-function binSequence()
+function seqs = binSequence(x, times, N)
+%{
+    Inputs:
+            x: some variable (such as event, avalanche size, branch)
+    times: times when the value x is measured
+           N: N is total number of time-points
+
+
+    Outputs:
+        seqs: cell of all elements of x at each time-point
+
+%}
+
+    seqs = cell(N,1);
+    for i = 1:numel(x)
+        seqs(times) = [seqs(times(i)), x(i)];
+    end
+
+
+end
