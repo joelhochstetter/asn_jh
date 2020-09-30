fa918efcf4e8c039f5541b7b0f8d60bf0ab27ac3
joelhochstetter <joel.hochstetter97@gmail.com>
2020-09-30 11:42:22 +1000

    Update files

--- experiments/network/DC_sweep_vary_netsVG.m
+++ experiments/network/DC_sweep_vary_netsVG.m
@@ -9,7 +9,6 @@ function DC_sweep_vary_netsVG(idx, netFolder, saveFolder)
 %     DC_Vsweep_for_cluster(Vidx, saveF1, 45*1.25*0.01, 45*1.25*0.01, 45*0.25*0.01, nets(netIdx).name, 0 , '.', 45)
 %      DC_Vsweep_for_cluster(Vidx, saveF1, 45*5*0.01, 2.5, 1.0, nets(netIdx).name, 0 , '.', 45, 10, 0)
  %    DC_Vsweep_for_cluster(Vidx, saveF1, 45*0.25*0.01, 45*2.00*0.01, 45*0.25*0.01, nets(netIdx).name, 'tl_T10_DC2.25V_s0.01_r0.005_c0.01_m0.015_b2_p1.mat', saveF1, 45, 45, 1)
-    DC_Vsweep_for_cluster(Vidx, saveF1, 0.5*0.01, 2.0*0.01, 0.02*0.01, nets(netIdx).name, 0 , '.', -1, 800, false, true, 0, 1, false, 0, '', 0, false);
-     
+    DC_Vsweep_for_cluster(Vidx, saveF1, 0.5*0.01, 2.0*0.01, 0.02*0.01, nets(netIdx).name, 0 , '.', -1, 800, false, true, 0, 1, false, 0, '', 0, false);     
      
 end
